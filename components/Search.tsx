5f1d82abab0054ea9e45ffc0b1f98a1110a7a36f
Abdullah-Nass <abdallahalk8@gmail.com>
2026-08-16 22:45:43 +0300

    add interaction

--- components/Search.tsx
+++ components/Search.tsx
@@ -116,7 +116,7 @@ export default function Search() {
                   setQuery("");
                   setSelectedIndex(-1);
                 }}
-                className={`flex w-full items-center gap-3 px-4 py-2 text-left text-sm text-neutral-200 transition-colors hover:bg-neutral-800 ${
+                className={`flex w-full items-center gap-3 px-4 py-2 text-left text-sm text-neutral-200 transition-colors hover:bg-neutral-800  active:bg-neutral-800 ${
                   selectedIndex === index
                     ? "bg-neutral-800 ring-1 ring-inset ring-emerald-500/50"
                     : ""
